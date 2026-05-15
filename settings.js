9ee8b29714e672e6434dec8bc15374bf93ccbc37
kk241228 <didada9898@163.com>
2026-05-15 19:54:07 +0800

    上传完整小游戏

---
+++ settings.js
@@ -0,0 +1,37 @@
+window.__settings__ = {
+    "debug": false,
+    "designResolution": {
+        "width": 320,
+        "height": 640,
+        "fitWidth": false,
+        "fitHeight": true
+    },
+    "launchScene": "",
+    "jsList": [],
+    "bundleVers": {},
+    "server": "",
+    "md5Cache": false,
+    "subpackages": [],
+    "preloadBundles": [],
+    "hotUpdate": {
+        "enabled": false
+    }
+};window.__settings__ = {
+    "debug": false,
+    "designResolution": {
+        "width": 320,
+        "height": 640,
+        "fitWidth": false,
+        "fitHeight": true
+    },
+    "launchScene": "",
+    "jsList": [],
+    "bundleVers": {},
+    "server": "",
+    "md5Cache": false,
+    "subpackages": [],
+    "preloadBundles": [],
+    "hotUpdate": {
+        "enabled": false
+    }
+};
