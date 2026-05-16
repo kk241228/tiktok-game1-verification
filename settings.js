9b3d9fb129e5815d5bb78782671b16a0e4e8ba09
kk241228 <didada9898@163.com>
2026-05-16 19:31:50 +0800

    24

--- settings.js
+++ settings.js
@@ -1,28 +1,26 @@
 window._CCSettings = {
     platform: "web-mobile",
-    groupList: ["default"],
+    groupList: [
+        "default"
+    ],
     collisionMatrix: [],
-    server: {
-        base: "./",
-        cdn: "./",
-        md5AssetsMap: {},
-        bundleVers: {}
+    rawAssets: {
+        assets: {},
+        internal: {}
     },
-    launchScene: "db://assets/scene/loading.fire",
+    launchScene: "db://assets/Start.scene",
     designResolution: {
         width: 720,
         height: 1280
     },
-    fitWidth: true,
-    fitHeight: false,
-    adaptScreen: true,
-    autoFit: false,
-    isLandscape: false,
     debug: false,
     showFPS: false,
     frameRate: 60,
-    orientation: {
-        landscape: false,
-        portrait: true
+    registerGlobal: true,
+    adapter: {
+        canvas: {
+            fitWidth: true,
+            fitHeight: true
+        }
     }
 };
